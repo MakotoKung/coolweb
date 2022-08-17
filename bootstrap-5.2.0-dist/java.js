5d8590b0da8e057002ff5c54bb0afd5d354af492
Shifuro <89166876+MakotoKung@users.noreply.github.com>
2022-08-17 16:03:49 +0700

    asdw

--- bootstrap-5.2.0-dist/java.js
+++ bootstrap-5.2.0-dist/java.js
@@ -4,6 +4,7 @@ var main =document.getElementById('main');
 var text = document.getElementById('text');
 var bt = document.getElementById('act');
 var rec = document.getElementById('rec');
+var bt = document.getElementById('bt');
 
     
 
@@ -12,5 +13,6 @@ btn.onclick = function(){
     bg.classList.toggle('op0');  
     main.classList.toggle('lowmain');  
     text.classList.toggle('recmenu');  
+    bt.classList.toggle('opacity');
     
 }
