c982f47173d672c9e0750298536721ae1845180c
Shifuro <89166876+MakotoKung@users.noreply.github.com>
2022-08-17 11:30:12 +0700

    ุุถ

--- bootstrap-5.2.0-dist/java.js
+++ bootstrap-5.2.0-dist/java.js
@@ -1,8 +1,18 @@
 var btn = document.getElementById('active');
+var a = document.getElementById('2')
 var bg = document.getElementById('background');
 var main =document.getElementById('main');
+var text = document.getElementById('text')
+var bg2 = document.getElementById('background2');
+    
+
 
 btn.onclick = function(){
     bg.classList.toggle('op0');  
-    main.classList.toggle('lowmain');       
+    main.classList.toggle('lowmain');  
+    text.classList.toggle('recmenu');     
 }
+a.onclick = function(){
+    bg2.classList.toggle('op0');        
+
+}   
