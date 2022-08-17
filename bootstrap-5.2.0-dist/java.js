f14d1dd54b9562e16bedcc177cfa1399decb8bda
Shifuro <89166876+MakotoKung@users.noreply.github.com>
2022-08-17 15:15:22 +0700

    llo

--- bootstrap-5.2.0-dist/java.js
+++ bootstrap-5.2.0-dist/java.js
@@ -1,18 +1,16 @@
 var btn = document.getElementById('active');
-var a = document.getElementById('2')
 var bg = document.getElementById('background');
 var main =document.getElementById('main');
 var text = document.getElementById('text');
-var bg2 = document.getElementById('background2');
+var bt = document.getElementById('act');
+var rec = document.getElementById('rec');
+
     
 
 
 btn.onclick = function(){
     bg.classList.toggle('op0');  
     main.classList.toggle('lowmain');  
-    text.classList.toggle('recmenu');     
+    text.classList.toggle('recmenu');  
+    
 }
-a.onclick = function(){
-    bg2.classList.toggle('op0');        
-
-}   
---
+++ java2.js
@@ -0,0 +1,6 @@
+var act = document.getElementById('act');
+var rec = document.getElementById('rec');
+
+act.onclick = function(){
+    rec.classList.remove('op0');
+}
